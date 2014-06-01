be2d28f818c06a6030d5ef89a916adeb6f3b145a
Taras Glek <github@glek.net>
2014-06-01 01:47:10 +0000

    docker now runs s3 thingy + varnish

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-/usr/sbin/varnishd -P /var/run/varnishd.pid -a :6081 -T localhost:6082 -u varnish -g varnish -f /etc/varnish/default.vcl -S /etc/varnish/secret -s file,/var/lib/varnish//varnish_storage.bin,200M -F
+#/usr/sbin/varnishd -P /var/run/varnishd.pid -a :6081 -T localhost:6082 -u varnish -g varnish -f /etc/varnish/default.vcl -S /etc/varnish/secret -s file,/var/lib/varnish//varnish_storage.bin,200M -F
+/etc/init.d/varnish start
+cd reverse-proxy-primer.js && nodejs index.js config/server.json
 
